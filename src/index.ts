d361941dc6b6ec4a6e0510662952aeb10ae8ed15
Gabriek0 <gabriel.sanches170@gmail.com>
2022-11-02 13:27:53 -0300

    feat: add http methods

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,33 @@ import express, { Express, response } from "express";
 
 const app: Express = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World!" });
+/**
+ *  GET -> get informations
+ *  POST -> post informations
+ *  PUT -> update informations
+ *  PATCH ->  update one piece of specific information
+ *  DELETE -> delete informations
+ *
+ */
+
+app.get("/courses", (req, res) => {
+  return res.json(["Course 1", "Course 2", "Course 3"]);
+});
+
+app.post("/courses", (req, res) => {
+  return res.json(["Course 1", "Course 2", "Course 3", "Course 4"]);
+});
+
+app.put("/courses/:id", (req, res) => {
+  return res.json(["Course 10", "Course 2", "Course 3"]);
+});
+
+app.patch("/courses/:id", (req, res) => {
+  return res.json(["Course 10", "Course 2", "Course 3"]);
+});
+
+app.delete("/courses/:id", (req, res) => {
+  return res.json(["Course 1", "Course 2"]);
 });
 
 app.listen(3333);
