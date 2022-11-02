5ae274afe1d0b720417362b36600dbc2640e8d63
Gabriek0 <gabriel.sanches170@gmail.com>
2022-11-02 13:44:26 -0300

    feat: parameter types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-import express, { Express, response } from "express";
+import express, { Express } from "express";
 
 const app: Express = express();
+
+// Middlewares
+app.use(express.json());
 
 /**
  *  GET -> get informations
@@ -8,18 +11,34 @@ const app: Express = express();
  *  PUT -> update informations
  *  PATCH ->  update one piece of specific information
  *  DELETE -> delete informations
+ */
+
+/**
+ * Parameter Types:
  *
+ * Route Params => Identify a resource (edit, delete and search);
+ * Query Patams => Pagination / Filter;
+ * Body Params => The objects to insertion and change (JSON);
  */
 
 app.get("/courses", (req, res) => {
+  const query = req.query;
+  console.log(query);
+
   return res.json(["Course 1", "Course 2", "Course 3"]);
 });
 
 app.post("/courses", (req, res) => {
+  const body = req.body;
+  console.log(body);
+
   return res.json(["Course 1", "Course 2", "Course 3", "Course 4"]);
 });
 
 app.put("/courses/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+
   return res.json(["Course 10", "Course 2", "Course 3"]);
 });
 
